01c69611a1b26c654e90e291e8b9c156035fccb9
Erjik1 <120021425+Erjik1@users.noreply.github.com>
2023-04-23 17:22:29 +0600

    hw1-erjan

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import MainPage from "./pages/MainPage";
+import AboutPage from "./pages/AboutPage";
+import ContactsPage from "./pages/ContactsPage";
+import PostsPage from "./pages/PostsPage";
+import {NavBar} from "./components/NavBar";
+import FooterPage from "./pages/FooterPage";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <NavBar/>
+            <Routes>
+                <Route index element={<MainPage/>}/>
+                <Route path="posts" element={<PostsPage/>}/>
+                <Route path="about" element={<AboutPage/>}/>
+                <Route path="contacts" element={<ContactsPage/>}/>
+                <Route path="footer" element={<FooterPage/>}/>
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
-export default App;
+export default App
